f67d75a163a572238714c90f42a0b6462448326a
cosmincostea12 <cosmincostea0810@gmail.com>
2025-11-06 17:23:18 +0100

    Formulario-EmailJS

---
+++ js/contacto.js
@@ -0,0 +1,18 @@
+    (function() {
+      // Inicializar EmailJS
+      emailjs.init("TqFs5BswIM5YMch1k"); // ⚠️ Cambia por tu Public Key (antes se llamaba user_id)
+    })();
+
+    // Escuchar el envío del formulario
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault(); // Evita que recargue la página
+
+      emailjs.sendForm('service_7r7fpcd', 'template_xypm3rf', this)
+        .then(() => {
+          alert('✅ Correo enviado correctamente');
+          this.reset();
+        }, (error) => {
+          console.error('Error:', error);
+          alert('Hubo un problema al enviar el correo');
+        });
+    });
